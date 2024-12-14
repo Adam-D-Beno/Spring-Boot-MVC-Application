a190a5351edd2331fbe9bdcf08764b206248a688
ПК_Home <wrofdt@ya.ru>
2024-12-14 23:15:13 +0300

    refactor PetControllerTest

---
+++ src/test/java/org/das/springmvc/controller/PetControllerTest.java
@@ -0,0 +1,28 @@
+package org.das.springmvc.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PetControllerTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void updateById() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+}
