8cc42e12747367d393b7992ca355c27b1a6baaed
ПК_Home <wrofdt@ya.ru>
2024-12-14 20:32:33 +0300

    create handleValidationException UserController

--- src/main/java/org/das/springmvc/exception/GlobalExceptionHandler.java
+++ src/main/java/org/das/springmvc/exception/GlobalExceptionHandler.java
@@ -1,4 +1,42 @@
 package org.das.springmvc.exception;
 
+import org.das.springmvc.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+    private final static Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessageResponse> handleValidationException(MethodArgumentNotValidException e) {
+        LOGGER.error("Got validation exception: MethodArgumentNotValidException", e);
+        String detailMessage = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + " : "
+                        + error.getDefaultMessage() + "."
+                        + " Отклоненное значение: " + error.getRejectedValue())
+                .collect(Collectors.joining(", "));
+
+        var errorDto = new ErrorMessageResponse(
+                "Ошибка валидации запроса",
+                detailMessage,
+                LocalDateTime.now()
+        );
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(errorDto);
+    }
+
 }
