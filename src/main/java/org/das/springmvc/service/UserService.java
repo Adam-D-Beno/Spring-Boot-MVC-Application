719b721dbb0946ee58473bcc811ee06fd03b09c9
ПК_Home <wrofdt@ya.ru>
2024-12-14 13:56:37 +0300

    refactor UserService

--- src/main/java/org/das/springmvc/service/UserService.java
+++ src/main/java/org/das/springmvc/service/UserService.java
@@ -2,9 +2,10 @@ package org.das.springmvc.service;
 
 import org.das.springmvc.model.Pet;
 import org.das.springmvc.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.*;
 
 @Service
@@ -13,6 +14,7 @@ public class UserService {
     private final Map<Long, User> userMap;
     private Long idUserCounter;
     private final PetService petService;
+    private final static Logger LOGGER = LoggerFactory.getLogger(UserService.class);
 
     @Autowired
     public UserService(PetService petService) {
@@ -23,13 +25,11 @@ public class UserService {
 
     public User create(User userToCreate) {
         var newUserId = ++idUserCounter;
-        var pets = userToCreate.getPets();
-        if (pets != null) {
-            pets = petService.create(pets);
-        } else {
-            pets = Collections.emptyList();
-        }
+        LOGGER.info("execute method Create in UserService, user: user={} and id: id={}", userToCreate, newUserId);
 
+        List<Pet> pets = userToCreate.isPetsEmpty()
+                ? new ArrayList<>()
+                : petService.create(userToCreate.getPets(), newUserId);
         var newUser = new User(
                 idUserCounter,
                 userToCreate.getName(),
@@ -37,23 +37,52 @@ public class UserService {
                 userToCreate.getAge(),
                 pets
         );
-
         this.userMap.put(newUserId, newUser);
         return newUser;
     }
 
     public User updateById(Long id, User userToUpdate) {
-        return null;
+        LOGGER.info("execute method updateById in UserService, user: user={} and id: id={}", userToUpdate, id);
+        User user = Optional.ofNullable(userMap.get(id))
+                .orElseThrow(() -> new NoSuchElementException("No such user with id=%s not found"
+                        .formatted(id)));
+
+        if (userToUpdate.isPetsEmpty()) {
+            user.setName(userToUpdate.getName());
+            user.setEmail(userToUpdate.getEmail());
+            user.setAge(userToUpdate.getAge());
+        } else {
+            List<Pet> pets = petService.create(userToUpdate.getPets(), user.getId());
+            user.setName(userToUpdate.getName());
+            user.setEmail(userToUpdate.getEmail());
+            user.setAge(userToUpdate.getAge());
+            user.addPets(pets);
+        }
+        userMap.put(id, user);
+        return user;
     }
 
     public void deleteById(Long id) {
+        LOGGER.info("execute method deleteById in UserService, id: id={}", id);
+        Optional.ofNullable(userMap.remove(id))
+                .orElseThrow(() -> new NoSuchElementException("No such user with id=%s not found"
+                        .formatted(id)));
     }
 
     public List<User> findAll(String name, String email) {
-        return null;
+        LOGGER.info("execute method findAll in UserService, for user with name: name={}, email: email={}"
+                ,name ,email);
+        return  userMap.values()
+                .stream()
+                .filter(user -> Objects.isNull(name) || user.getName().equals(name))
+                .filter(user ->Objects.isNull(email) || user.getEmail().equals(email))
+                .toList();
     }
 
     public User findById(Long id) {
-        return null;
+        LOGGER.info("execute method findById in UserService, id: id={}", id);
+        return Optional.ofNullable(userMap.get(id))
+                .orElseThrow(() -> new NoSuchElementException("No such user with id=%s not found"
+                        .formatted(id)));
     }
 }
