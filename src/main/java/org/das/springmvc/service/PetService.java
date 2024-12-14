ee48cacfdf146954c82ff3afeb759f37370716d1
ПК_Home <wrofdt@ya.ru>
2024-12-14 13:56:28 +0300

    refactor PetService

--- src/main/java/org/das/springmvc/service/PetService.java
+++ src/main/java/org/das/springmvc/service/PetService.java
@@ -3,17 +3,19 @@ package org.das.springmvc.service;
 import jakarta.annotation.PostConstruct;
 import org.das.springmvc.model.Pet;
 import org.das.springmvc.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class PetService {
 
     private final Map<Long, Pet> petMap;
     private Long idPetCounter;
+    private final static Logger LOGGER = LoggerFactory.getLogger(PetService.class);
+
     public PetService() {
         this.petMap = new HashMap<>();
         this.idPetCounter = 0L;
@@ -21,31 +23,56 @@ public class PetService {
 
     public Pet create(Pet petToCreate) {
         var newPetId = ++idPetCounter;
+        LOGGER.info("execute method Create in PetService, pet: pet={} and id: id={}", petToCreate, newPetId);
         var newPet = new Pet(
-                newPetId, petToCreate.getName(),petToCreate.getUserId()
+                newPetId, petToCreate.getName(), petToCreate.getUserId()
         );
         this.petMap.put(newPetId, newPet);
         return newPet;
     }
-    public List<Pet> create(List<Pet> pets) {
-       return pets.stream()
-                .map(pet -> new Pet(++idPetCounter, pet.getName(), pet.getUserId()))
-                .toList();
 
+    //todo check exist user
+    public List<Pet> create(List<Pet> pets, Long userId) {
+        LOGGER.info("execute method Create in PetService, pets: pets={}", pets);
+        return pets.stream()
+                .map(pet -> new Pet(++idPetCounter, pet.getName(), userId))
+                .toList();
     }
 
     public Pet updateById(Long id, Pet petToUpdate) {
-        return null;
+        LOGGER.info("execute method updateById in PetService, pet: pet={} and id: id={}", petToUpdate, id);
+        Pet pet = Optional.ofNullable(petMap.get(id))
+                .orElseThrow(() -> new NoSuchElementException("No such pet with id=%s not found"
+                        .formatted(id)));
+
+        pet.setName(petToUpdate.getName());
+        pet.setUserId(petToUpdate.getUserId());
+
+        petMap.put(id, pet);
+        return pet;
     }
 
     public void deleteById(Long id) {
+        LOGGER.info("execute method deleteById in PetService, id: id={}", id);
+        Optional.ofNullable(petMap.remove(id))
+                .orElseThrow(() -> new NoSuchElementException("No such pet with id=%s not found"
+                        .formatted(id)));
     }
 
     public List<Pet> findAll(String name, Long userId) {
-        return null;
+        LOGGER.info("execute method findAll in PetService, for pet with name: name={}, userId: userId={}"
+                , name, userId);
+        return petMap.values()
+                .stream()
+                .filter(pet -> Objects.isNull(name) || pet.getName().equals(name))
+                .filter(pet -> Objects.isNull(userId) || pet.getUserId().equals(userId))
+                .toList();
     }
 
     public Pet findById(Long id) {
-        return null;
+        LOGGER.info("execute method findById in PetService, for pet with id: id={}", id);
+        return Optional.ofNullable(petMap.get(id))
+                .orElseThrow(() -> new NoSuchElementException("No such pet with id=%s not found"
+                        .formatted(id)));
     }
 }
