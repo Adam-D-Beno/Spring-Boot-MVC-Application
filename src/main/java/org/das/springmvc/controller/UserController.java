5563b722fef38719d1d0ce767cdc850b00c7329c
ПК_Home <wrofdt@ya.ru>
2024-12-14 19:06:33 +0300

    refactor UserController

--- src/main/java/org/das/springmvc/controller/UserController.java
+++ src/main/java/org/das/springmvc/controller/UserController.java
@@ -7,6 +7,8 @@ import org.das.springmvc.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,38 +25,54 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public User create(@RequestBody @Valid User userToCreate) {
+    public ResponseEntity<User> create(@RequestBody @Valid User userToCreate) {
         LOGGER.info("Get request in UserController for created user: user={}", userToCreate);
-        return userService.create(userToCreate);
+        User user = userService.create(userToCreate);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(user);
     }
 
     @PutMapping("/users/{id}")
-    public User updateById(
+    public ResponseEntity<User> updateById(
            @PathVariable @NotNull Long id,
            @RequestBody @Valid User userToUpdate) {
-        LOGGER.info("Get request in UserController update for user with id: id={}, user: user={}", id, userToUpdate);
-        return userService.updateById(id, userToUpdate);
+        LOGGER.info("Get request in UserController update for user with id: id={}, user: user={}"
+                ,id ,userToUpdate);
+        User user = userService.updateById(id, userToUpdate);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(user);
     }
 
     @DeleteMapping("/users/{id}")
-    public void deleteById(@PathVariable @NotNull Long id) {
+    public ResponseEntity<Void> deleteById(@PathVariable @NotNull Long id) {
         LOGGER.info("Get request in UserController delete for user with id: id={}", id);
         userService.deleteById(id);
+        return ResponseEntity
+                .status(HttpStatus.NO_CONTENT)
+                .build();
     }
 
     @GetMapping("/users")
-    public List<User> findAll(
+    public ResponseEntity<List<User>> findAll(
            @RequestParam(value = "name", required = false) String name,
            @RequestParam(value = "email", required = false) String email
     ) {
         LOGGER.info("Get request in UserController findAll for user with name: name={}, email: email={}"
                 ,name ,email);
-        return userService.findAll(name, email);
+        List<User> users = userService.findAll(name, email);
+        return ResponseEntity
+                .status(HttpStatus.FOUND)
+                .body(users);
     }
 
     @GetMapping("/users/{id}")
-    public User findById(@PathVariable @NotNull Long id) {
+    public ResponseEntity<User> findById(@PathVariable @NotNull Long id) {
         LOGGER.info("Get request in UserController find for user with id: id={}", id);
-        return userService.findById(id);
+        User user = userService.findById(id);
+        return ResponseEntity
+                .status(HttpStatus.FOUND)
+                .body(user);
     }
 }
