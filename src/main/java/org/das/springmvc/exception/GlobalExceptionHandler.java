d62ecc79045cf7c335bcabd67f02914078cb1919
ПК_Home <wrofdt@ya.ru>
2024-12-17 09:02:27 +0300

    refactor GlobalExceptionHandler.java

--- src/main/java/org/das/springmvc/exception/GlobalExceptionHandler.java
+++ src/main/java/org/das/springmvc/exception/GlobalExceptionHandler.java
@@ -6,12 +6,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.method.MethodValidationException;
+import org.springframework.validation.method.ParameterErrors;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
@@ -40,17 +45,23 @@ public class GlobalExceptionHandler {
                 .body(errorDto);
     }
 
-    @ExceptionHandler
-    public ResponseEntity<ErrorMessageResponse> handleGenericException(Exception e) {
-        LOGGER.error("Got validation exception: ", e);
-        var error = new ErrorMessageResponse(
-                "Server error",
-                e.getMessage()
-                ,LocalDateTime.now()
+    @ExceptionHandler(HandlerMethodValidationException.class)
+    public ResponseEntity<ErrorMessageResponse> HandlerMethodValidationException(HandlerMethodValidationException e) {
+        LOGGER.error("Got validation exception: HandlerMethodValidationException", e);
+        String detailMessage = e.getBeanResults().stream()
+                .flatMap(parameterErrors -> parameterErrors.getFieldErrors().stream())
+                .map(error -> String.format("%s : %s. Отклоненное значение: %s",
+                        error.getField(), error.getDefaultMessage(), error.getRejectedValue()))
+                .collect(Collectors.joining(", "));
+
+        var errorDto = new ErrorMessageResponse(
+                "Ошибка валидации запроса",
+                detailMessage,
+                LocalDateTime.now()
         );
         return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(error);
+                .status(HttpStatus.BAD_REQUEST)
+                .body(errorDto);
     }
 
     @ExceptionHandler(NoSuchElementException.class)
@@ -63,6 +74,19 @@ public class GlobalExceptionHandler {
         );
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
+                .body(error);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorMessageResponse> handleGenericException(Exception e) {
+        LOGGER.error("You in handleGenericException. Got validation exception: {}", e.getMessage());
+        var error = new ErrorMessageResponse(
+                "Server error",
+                e.getMessage()
+                ,LocalDateTime.now()
+        );
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(error);
     }
 }
