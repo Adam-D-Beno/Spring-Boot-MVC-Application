ddd567de6405998e391d79f754dec100936e2918
ПК_Home <wrofdt@ya.ru>
2024-12-18 23:37:39 +0300

    refactor PetMapper

--- src/main/java/org/das/springmvc/dto/PetMapper.java
+++ src/main/java/org/das/springmvc/dto/PetMapper.java
@@ -1,16 +1,34 @@
 package org.das.springmvc.dto;
 //todo export in main branch
 import org.das.springmvc.model.Pet;
+import org.springframework.stereotype.Component;
 
+import java.util.List;
+
+@Component
 public class PetMapper implements Mapper<Pet, PetDto> {
 
     @Override
     public PetDto toDto(Pet pet) {
-        return null;
+        return new PetDto(
+                pet.id(),
+                pet.name(),
+                pet.userId()
+        );
+    }
+
+    @Override
+    public List<PetDto> toDto(List<Pet> pets) {
+        return pets.stream().map(this::toDto).toList();
     }
 
     @Override
     public Pet toEntity(PetDto petDto) {
-        return null;
+        return new Pet(
+                petDto.id(),
+                petDto.name(),
+                petDto.userId()
+        );
     }
+
 }
