54448921a2af86b500aaebf028b8dd40665eee27
ПК_Home <wrofdt@ya.ru>
2024-12-19 08:27:59 +0300

    refactor PetDto

--- src/main/java/org/das/springmvc/dto/PetDto.java
+++ src/main/java/org/das/springmvc/dto/PetDto.java
@@ -3,8 +3,18 @@ package org.das.springmvc.dto;
 
 //todo export in main branch
 
+import jakarta.validation.constraints.*;
+
 public record PetDto(
+        @Null(message = "Filed user id must be null")
         Long id,
+
+        @Size(max = 50, message = "Filed size user name must be max=50")
+        @NotBlank(message = "Filed user name must be not null or blank")
         String name,
+
+        @NotNull(message = "Filed user age must be not null")
+        @Digits(integer = Integer.MAX_VALUE, fraction = 0,
+                message = "Filed value userId is an integer with a maximum")
         Long userId
 ) {}
