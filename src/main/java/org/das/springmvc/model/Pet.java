774c39f0ea020ab752a39db025afacdc53451218
ПК_Home <wrofdt@ya.ru>
2024-12-19 07:49:02 +0300

    refactor model

--- src/main/java/org/das/springmvc/model/Pet.java
+++ src/main/java/org/das/springmvc/model/Pet.java
@@ -1,22 +1,11 @@
 package org.das.springmvc.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.validation.constraints.*;
 //todo export in main branch
 public record Pet(
-        @Null(message = "Filed user id must be null")
-        Long id,
-
-       @Size(max = 50, message = "Filed size user name must be max=50")
-       @NotBlank(message = "Filed user name must be not null or blank")
+       Long id,
        String name,
-
-       @NotNull(message = "Filed user age must be not null")
-       @Digits(integer = Integer.MAX_VALUE, fraction = 0,
-                        message = "Filed value userId is an integer with a maximum")
        Long userId
 ) {
-    @JsonIgnore
     public boolean isUserIdEmpty() {
         return userId == null;
     }
--- src/main/java/org/das/springmvc/model/User.java
+++ src/main/java/org/das/springmvc/model/User.java
@@ -1,35 +1,15 @@
 package org.das.springmvc.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import jakarta.validation.constraints.*;
 import java.util.ArrayList;
 import java.util.List;
 
-
-@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record User(
-        @Null(message = "Filed user id must be null")
-         Long id,
-
-        @Size(max = 50, message = "Filed size user name must be max=50")
-        @NotBlank(message = "Filed user name must be not null or blank")
+        Long id,
         String name,
-
-        @Size(max = 50, message = "Filed user email must be max=50")
-        @Email(message = "Filed user email must be email format")
-        @NotBlank(message = "Filed user email must be not null or blank")
         String email,
-
-        @Min(value = 1, message = "Filed value user age must be min=1 and max=100")
-        @Max(value = 100, message = "Filed value user age must be min=1 and max=100")
-        @NotNull(message = "Filed user age must be not null")
-        @Digits(integer = 3, fraction = 0, message = "Filed value user age  is an integer with a maximum of 3 digits")
         Integer age,
-
         List<Pet> pets
 ) {
-    @JsonIgnore
     public boolean isPetsEmpty() {
         return pets == null || pets.isEmpty();
     }
